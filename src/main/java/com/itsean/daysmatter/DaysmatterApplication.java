f1635d1b55331e7bfc4034e977ad267cd8dc1156
Original&Player <zhaosiyao0123@126.com>
2025-11-22 21:38:22 +0800

    Initial commit

---
+++ src/main/java/com/itsean/daysmatter/DaysmatterApplication.java
@@ -0,0 +1,13 @@
+package com.itsean.daysmatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysmatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysmatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsean/daysmatter/DaysmatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsean.daysmatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaysmatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
